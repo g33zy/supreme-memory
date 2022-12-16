14da23a6b8c8bd89144a3acf5fc86531d359d374
g33zy <stevejgaz@gmail.com>
2022-12-16 14:27:43 -0600

    portfolio update

--- scripts.js
+++ scripts.js
@@ -1,3 +1,25 @@
+// const placeForResults = document.getElementById("results")
+
+// new URLSearchParams(window.location.search).forEach((value, name) => {
+  
+//   placeForResults.append(`${name} : ${value}`)
+//   placeForResults.append(document.createElement("br"))
+// })
+
+
+let burgerButton = document.querySelector(".hamburger")
+//assign var burgerButton value of button element w/ class hamburger
+let dropDown = document.querySelector("nav")
+//assign var nav 
+const navDrop = () => {
+    //print value of button in var to screen
+    console.log(burgerButton)
+    burgerButton.classList.toggle('x-style');
+    //print value of nav in var to screen
+    console.log(dropDown)
+    dropDown.classList.toggle('nav-style')
+}
+
 const placeForResults = document.getElementById("results")
 
 new URLSearchParams(window.location.search).forEach((value, name) => {
@@ -5,3 +27,26 @@ new URLSearchParams(window.location.search).forEach((value, name) => {
   placeForResults.append(`${name} : ${value}`)
   placeForResults.append(document.createElement("br"))
 })
+
+
+
+
+const endpoint = 'http://www.boredapi.com/api/activity/';
+const button = document.querySelector('.button');
+const form = document.querySelector('#form');
+const activityWrapper = document.querySelector('.activity');
+const typeWrapper = document.querySelector('.type');
+
+const getActivity = async (event) => {
+  event.preventDefault();
+  const isFree = event.target.children.namedItem('free').checked;
+
+  let Api = isFree ? `${endpoint}?price=0` : endpoint;
+  const response = await fetch(Api);
+  const json = await response.json();
+  const {activity, type} =json;
+  activityWrapper.innerHTML = activity;
+}
+
+form.addEventListener("submit", getActivity)
+
