c37c2335019e8bd8d9cf6d3e9449a87460a5045e
g33zy <stevejgaz@gmail.com>
2022-12-19 17:17:45 -0600

    final touches

--- scripts.js
+++ scripts.js
@@ -50,3 +50,25 @@ const getActivity = async (event) => {
 
 form.addEventListener("submit", getActivity)
 
+
+
+function animate(options) {
+
+  var start = performance.now();
+
+  requestAnimationFrame(function animate(time) {
+    // timeFraction 0 1
+    var timeFraction = (time - start) / options.duration;
+    if (timeFraction > 1) timeFraction = 1;
+
+    
+    var progress = options.timing(timeFraction)
+    
+    options.draw(progress);
+
+    if (timeFraction < 1) {
+      requestAnimationFrame(animate);
+    }
+
+  });
+}
